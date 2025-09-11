b2e4d80703721665f7b241ea09f63f8c5d70ae98
Ted Shaffer <shaffer.family@gmail.com>
2025-09-11 07:21:07 -0700

    functional getForecast handler on server.

--- backend/src/controllers/app.ts
+++ backend/src/controllers/app.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
 import { version } from '../version';
+import { getDailyForecast } from '../utilities';
 
 export const getVersion = (request: Request, response: Response, next: any) => {
   const data: any = {
@@ -9,17 +10,19 @@ export const getVersion = (request: Request, response: Response, next: any) => {
   response.json(data);
 };
 
-export const getForecast = (request: Request, response: Response, next: any) => {
+export const getForecast = async (request: Request, response: Response, next: any) => {
   console.log('getForecast called with query:', request.query);
   const { location, date } = request.query as { location: string, date: string };
   const googleLocation = JSON.parse(location) as { lat: number; lng: number };
   console.log('Parsed location:', googleLocation);
+
+  const days = await getDailyForecast(googleLocation.lat, googleLocation.lng, 10);
+  console.log('Forecast days:', days);
+
   // Here you would normally call your weather API with the location and date
   // For this example, we'll just return a mock response
   return response.json({
-    location,
-    date,
-    forecast: "Sunny with a chance of code",
+    days,
   });
 }
 
